65d8e51335269a6509778abc78b6888cac85aff8
ytgeng99 <eric.genng@gmail.com>
2018-11-24 22:07:16 -0800

    Django Multiapps

---
+++ apps/surveys/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    response = 'placeholder to display all the surveys created'
+    return HttpResponse(response)
+
+def new(request):
+    response = 'placeholder for users to add a new survey'
+    return HttpResponse(response)
---
+++ apps/users/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    response = 'placeholder to later display all the list of users'
+    return HttpResponse(response)
+
+def register(request):
+    response = 'placeholder for users to create a new user record'
+    return HttpResponse(response)
+
+def login(request):
+    response = 'placeholder for users to login'
+    return HttpResponse(response)
